03117ae9012713cdc9f2cd3b23c229ac76c78274
sgu6ena <sgu6ena@gmail.com>
2023-02-27 12:58:26 +0200

    cors

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import cors from 'cors';
 import typeRouter from "./types/router.js";
 import arealRouter from "./areal/router.js";
 import themeRouter from "./themes/router.js";
@@ -10,7 +11,13 @@ import fileUpload from "express-fileupload"
 
 const app = express()
 const port = 8000
+const allowedOrigins = ['http://localhost:3000','http://127.0.0.1:3000/'];
+const options = {
+  origin: allowedOrigins
+};
 
+
+app.use(cors(options));
 app.use(bodyParser.json());
 
 app.use(express.static('static'));
@@ -20,14 +27,24 @@ app.use('/api', typeRouter)
 app.use('/api', arealRouter)
 app.use('/api', themeRouter)
 app.use('/api', pokemonRouter)
+
+
+
+
 const url = 'mongodb://localhost:27017/pkm'; // урл для сервиса с mongodb
 //const url = 'mongodb+srv://sgu6ena:Kct9VvPVm3eyM6r@cluster0.l2p9zk1.mongodb.net/?retryWrites=true&w=majority'
+
+
+
+
 
 mongoose.set('strictQuery', true);
 
 async function startApp() {
   try {
-    await mongoose.connect(url, {}, ()=>{})
+    await mongoose.connect(url, {}, ()=>{
+      console.log('connected to base')
+    })
 
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
